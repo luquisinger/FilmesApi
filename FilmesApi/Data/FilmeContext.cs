578a739b8f0acd22bc942e520a2de070b03ff23e
Matheus Luchsinger <102822195+luquisinger@users.noreply.github.com>
2023-11-07 13:40:23 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,16 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContex : DbContext
+{
+    public FilmeContex(DbContextOptions<FilmeContex> opts)
+        :base(opts) 
+    {
+          
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+
+}
